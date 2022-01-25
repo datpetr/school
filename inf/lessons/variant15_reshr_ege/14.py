6dab1b175d76231dfdc1858cb826225d40767450
master <dat.petr@gmail.com>
2022-01-25 19:31:45 +0300

    _

---
+++ inf/lessons/variant15_reshr_ege/14.py
@@ -0,0 +1,15 @@
+def x_to_n(x, n):
+    st = ''
+    while x:
+        st += str(x % n)
+        x //= n
+
+    return st
+
+
+expression = 343 ** 5 + 7 ** 3 - 1
+
+for x in range(1000):
+    if x_to_n(expression - x, 7).count('6') == 12:
+        print(x)
+        break
