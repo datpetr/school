194f590401407bde9790f90e7bb3146de4ade555
master <dat.petr@gmail.com>
2022-01-25 21:04:32 +0300

    _

--- inf/lessons/variant15_reshr_ege/12.py
+++ inf/lessons/variant15_reshr_ege/12.py
@@ -2,8 +2,8 @@ st = '1' + '0' * 80
 
 while '10' in st or '1' in st:
     if '10' in st:
-        st.replace('10', '001', 1)
+        st = st.replace('10', '001', 1)
     elif '1' in st:
-        st.replace('1', '000', 1)
+        st = st.replace('1', '000', 1)
 
 print(st.count('0'))
