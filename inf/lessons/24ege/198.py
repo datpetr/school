50376aac3eeef4615d095a261423c827669d0ead
master <dat.petr@gmail.com>
2022-06-07 16:59:01 +0300

    _

---
+++ inf/lessons/24ege/198.py
@@ -0,0 +1,20 @@
+f = open('files/24-197.txt', 'r').readline()
+
+a_x_y = []
+a_z_y = []
+for i in 'XYZ':
+    a_x_y.append('X{}Y'.format(i))
+    a_z_y.append('Z{}Y'.format(i))
+
+for i in a_x_y:
+    f = f.replace(i, '*')
+
+for i in a_z_y:
+    f = f.replace(i, '*')
+
+for i in 'XYZ':
+    f = f.replace(i, ' ')
+
+a = f.split()
+lengts = [len(_) for _ in a]
+print(max(lengts))
