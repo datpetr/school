a7d5aaf774f0dac653d7cce4ce64cdb0b378a4fc
master <dat.petr@gmail.com>
2022-06-08 13:10:05 +0300

    _

--- inf/lessons/26ege/74.py
+++ inf/lessons/26ege/74.py
@@ -13,7 +13,7 @@ for i in range(n - 1):
     if a[i][0] == a[i + 1][0] and a[i][1] + 2 == a[i + 1][1]:
         count_current += 1
     else:
-        if a[i][1] != a[i + 1][1]:
+        if a[i][1] != a[i + 1][1] or a[i][0] != a[i + 1][0]:
             if mx < count_current:
                 mx = count_current
                 mx_row = a[i][0]
