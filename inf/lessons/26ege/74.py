174ea3c2a9dbac12b540df8ebfcda9f479322193
master <dat.petr@gmail.com>
2022-06-08 13:07:41 +0300

    _

--- inf/lessons/26ege/74.py
+++ inf/lessons/26ege/74.py
@@ -10,19 +10,13 @@ for _ in range(n):
 a.sort()
 
 for i in range(n - 1):
-    if a[i][0] == a[i + 1][0]:
-        if a[i][1] + 2 == a[i + 1][1]:
-            count_current += 1
-        else:
-            if a[i][1] != a[i + 1][1]:
-                if mx < count_current:
-                    mx = count_current
-                    mx_row = a[i][0]
-                count_current = 0
+    if a[i][0] == a[i + 1][0] and a[i][1] + 2 == a[i + 1][1]:
+        count_current += 1
     else:
-        if mx < count_current:
-            mx = count_current
-            mx_row = a[i][0]
-        count_current = 0
+        if a[i][1] != a[i + 1][1]:
+            if mx < count_current:
+                mx = count_current
+                mx_row = a[i][0]
+            count_current = 0
 
 print(mx + 1, mx_row)
