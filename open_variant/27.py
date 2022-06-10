e379defc7317c9fead9863c8b11f47ccab2fb18d
master <dat.petr@gmail.com>
2022-06-10 16:26:41 +0300

    _

--- open_variant/27.py
+++ open_variant/27.py
@@ -1,17 +1,16 @@
 from itertools import accumulate
 
+with open('files/27_B.txt') as f:
+    n = int(f.readline())
+    m = [int(f.readline()) for _ in range(n)] * 2
+    c = 3
 
-f = list(map(int, open('files/27_A.txt', 'r').readlines()))
-
-n = f.pop(0)
-a = f
-f = a + f
-pr = [0] + list(accumulate(f))
-s = sum(min(j, n - j) * f[j] * 3 for j in range(n))
-ms = s
+pr = [0] + list(accumulate(m))
+ms = 10 ** 100
+s = sum([min(n - k, k) * m[k] * c for k in range(n)])
 
 for i in range(1, n):
     s = s - (pr[i + n // 2] - pr[i]) + (pr[i + n] - pr[i + n // 2])
-    ms = min(ms, s)
+    ms = min(s, ms)
 
-print(ms * 3)
+print(ms)
