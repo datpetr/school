6ed64a39cecfecfffed986109f14852b2c9ea7de
master <dat.petr@gmail.com>
2022-06-15 17:25:29 +0300

    _

--- 06.22_probe/24.py
+++ 06.22_probe/24.py
@@ -1,11 +1,12 @@
 f = open('files/24_2.txt', 'r').readline()
 
-f = f.replace('BB', '*')
-f = f.replace('DD', '*')
-f = f.replace('A', ' ')
-f = f.replace('B', ' ')
-f = f.replace('D', ' ')
-print(f.split())
-lengths = [len(i) for i in f.split()]
+count, mx = 0, 0
+for j in range(2):
+    for i in range(j, len(f) - 1, 2):
+        if f[i] + f[i + 1] == 'BB' or f[i] + f[i + 1] == 'DD':
+            count += 1
+            mx = max(mx, count)
+        else:
+            count = 0
 
-print(max(lengths) + 1)
+print(mx)
